34045d9339a551d70947cf2a107d3731209d7b7e
Sahils Tab-s9 <sahilstabs@gmail.com>
2026-01-29 17:37:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3679153134-57562",
+  "appId": "1:356236579052:web:d3159e3a1bc1a5e55155c9",
+  "apiKey": "AIzaSyBO7ciGYj_R_376vPA_uSA_J538npAivaY",
+  "authDomain": "studio-3679153134-57562.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "356236579052"
+};
